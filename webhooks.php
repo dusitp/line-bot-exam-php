679ca90797ae450b697193a7db9ff6e70ec6e97c
dusitp <sit_silk@hotmail.com>
2019-07-25 08:36:23 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'miySQu2ueZVJeCrBUfKSbUZOoaM7/OdSikQE15LC+TVSTCQGp3lufNISDsHUsulQjU/X2yHkDMlV/IV4P82Ph1byF1bBWYSNil5Zs10Ddpk0BDcdRUYkDX7KbilKtw5UTvnndyUhDA5/qjdzDieQZgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
